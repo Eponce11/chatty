b69fe187ae53c118d11a0b6ad24edafe5dabd5ae
Eric Ponce <ponceeric458@gmail.com>
2023-08-07 22:37:30 -0700

    setup landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Landing } from "./pages";
+
 function App() {
-  return <div></div>;
+  return <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+      </Routes>
+    </Router>
+  </div>;
 }
 
 export default App;
---
+++ client/src/pages/Landing/index.tsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default Landing;
