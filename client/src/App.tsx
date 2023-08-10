1e3134fe8b9176000cd8bc82bc6c452e99ebfade
Eric <ponceeric458@gmail.com>
2023-08-10 13:32:55 -0700

    setup Landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import { Landing } from "./pages";
 
 function App() {
-  return <div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing/>} />
-      </Routes>
-    </Router>
-  </div>;
+  return (
+    <div className="App h-screen w-screen">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -1,9 +1,11 @@
+import { Navbar } from "./components";
+
 const Landing = () => {
   return (
-    <div>
-      Hello World
+    <div className="w-full h-[400px] bg-[blue]">
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
