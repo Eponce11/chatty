4b427a3343d51a7d0c7a5db73da8d4c69702b2ae
Eric Ponce <ponceeric458@gmail.com>
2023-08-11 23:18:04 -0700

    register component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Landing, Login } from "./pages";
+import { Landing, Login, Register } from "./pages";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </div>
