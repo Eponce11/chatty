4462f96f32dec42881592932445ee3298ef09323
Eric <ponceeric458@gmail.com>
2023-08-23 17:14:21 -0700

    home page component

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,6 @@
 import Landing from "./Landing";
 import Login from "./Login";
 import Register from "./Register";
+import Home from "./Home";
 
-export { Landing, Login, Register };
+export { Landing, Login, Register, Home};
