948768bad6c60f1d6d5c6e941013b141b920c6f1
Eric Ponce <ponceeric458@gmail.com>
2023-08-27 23:38:39 -0700

    server select component

---
+++ client/src/pages/Home/components/ServerSelect.tsx
@@ -0,0 +1,11 @@
+const ServerSelect = () => {
+  return (
+    <div className="h-full w-[72px]">
+      <ul>
+        <li className="h-[48px] w-[48px]"></li>
+      </ul>
+    </div>
+  );
+};
+
+export default ServerSelect;
