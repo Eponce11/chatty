c2f329dbe405927ca31dafe260b5f557f24fb33d
Eric Ponce <ponceeric458@gmail.com>
2023-09-24 22:04:21 -0700

    message text

--- client/src/pages/Home/components/MessageChannel.tsx
+++ client/src/pages/Home/components/MessageChannel.tsx
@@ -15,7 +15,10 @@ const MessageChannel = () => {
           return (
             <div className="flex">
               <div className="bg-[red] aspect-square h-[40px] rounded-full mr-3 mb-4" />
-              <span>Hello World</span>
+              <div>
+                <span className="text-[#CBCBCE] font-semibold">Username</span>
+                <p className="text-[#C1C5C7]">Hello World</p>
+              </div>
             </div>
           );
         })}
