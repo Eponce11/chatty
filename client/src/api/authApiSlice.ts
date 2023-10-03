81f791504c8b1f6afcb793f798740eec0de3e325
Eric Ponce <ponceeric458@gmail.com>
2023-10-02 22:26:12 -0700

    register rtk call

--- client/src/api/authApiSlice.ts
+++ client/src/api/authApiSlice.ts
@@ -12,6 +12,17 @@ interface LoginResponse {
 
 export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    register: builder.mutation<any, any>({
+      query: (data: any) => ({
+        url: '/user/register',
+        method: 'POST',
+        body: { ...data }
+      }),
+      transformResponse: (responseData: { data: any }) => {
+        return responseData.data
+      }
+    }),
+
     login: builder.mutation<LoginResponse, LoginCredentials>({
       query: (credentials: LoginCredentials) => ({
         url: "/user/login",
