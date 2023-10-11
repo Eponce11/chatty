8fc72f9de9ee7904fa7d48431ace07d528e2c7dd
Eric Ponce <ponceeric458@gmail.com>
2023-10-10 19:49:59 -0700

    auth slice

--- client/src/api/apiSlice.ts
+++ client/src/api/apiSlice.ts
@@ -1,9 +1,36 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials, logout } from "../app/features/authSlice";
+import { RootState } from "../app/store";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8000/api",
   credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`)
+    }
+    return headers
+  }
 })
+
+const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 403) {
+    console.log('sending refresh token')
+    const refreshResult = await baseQuery('/auth/refreshToken', api, extraOptions);
+    console.log(refreshResult);
+    if (refreshResult?.data) {
+      const id = api.getState().auth
+      // api.dispatch(setCredentials({ ...refreshResult.data,  }))
+      result = await baseQuery(args, api, extraOptions)
+    } else {
+      api.dispatch(logout())
+    }
+  }
+
+}
 
 export const apiSlice = createApi({
   reducerPath: "api",
--- client/src/app/features/authSlice.ts
+++ client/src/app/features/authSlice.ts
@@ -5,26 +5,36 @@ import type { RootState } from "../store";
 interface AuthState {
   id: string | null;
   username: string | null;
+  token: string | null
 }
 
 const initialState: AuthState = {
   id: null,
   username: null,
+  token: null,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setAuth: (state: AuthState, action: PayloadAction<AuthState>) => {
+    setCredentials: (state: AuthState, action: PayloadAction<AuthState>) => {
       state.id = action.payload.id;
       state.username = action.payload.username;
+      state.token = action.payload.token;
     },
+    logout: (state: AuthState, action: PayloadAction<void>) => {
+      state.id = null;
+      state.username = null;
+      state.token = null;
+    }
   },
 });
 
-export const { setAuth } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 
 export const selectAuthId = (state: RootState) => state.auth.id;
+export const selectAuthUsername = (state: RootState) => state.auth.username;
+export const selectAuthToken = (state: RootState) => state.auth.token;
 
 export default authSlice.reducer;
