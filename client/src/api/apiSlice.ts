668a2e3f59a67984be16e6ec38548141caf19008
Eric Ponce <ponceeric458@gmail.com>
2023-10-12 20:14:42 -0700

    reauth with refreshtoken

--- client/src/api/apiSlice.ts
+++ client/src/api/apiSlice.ts
@@ -4,37 +4,46 @@ import { RootState } from "../app/store";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8000/api",
-  credentials: 'include',
+  credentials: "include",
   prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as RootState).auth.token
+    const token = (getState() as RootState).auth.token;
     if (token) {
-      headers.set("authorization", `Bearer ${token}`)
+      headers.set("authorization", `Bearer ${token}`);
     }
-    return headers
-  }
-})
+    return headers;
+  },
+});
 
 const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
-  let result = await baseQuery(args, api, extraOptions);
-
-  if (result?.error?.status === 403) {
-    console.log('sending refresh token')
-    const refreshResult = await baseQuery('/auth/refreshToken', api, extraOptions);
-    console.log(refreshResult);
+  let result: any = await baseQuery(args, api, extraOptions);
+  if (result?.error?.originalStatus === 403) {
+    console.log("sending refresh token");
+    const refreshResult: any = await baseQuery(
+      "/auth/refreshToken",
+      api,
+      extraOptions
+    );
     if (refreshResult?.data) {
-      const id = api.getState().auth
-      // api.dispatch(setCredentials({ ...refreshResult.data,  }))
-      result = await baseQuery(args, api, extraOptions)
+      const id = api.getState().auth.id;
+      const username = api.getState().auth.username;
+      api.dispatch(
+        setCredentials({
+          id,
+          username,
+          token: refreshResult.data.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
     } else {
-      api.dispatch(logout())
+      api.dispatch(logout());
     }
   }
-
-}
+  return result;
+};
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithReauth,
   tagTypes: [],
-  endpoints: (builder) => ({}),
+  endpoints: () => ({}),
 });
