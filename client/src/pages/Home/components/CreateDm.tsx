725c56dd38069aeb4da9d9a9cf16f387c3fe6c68
Eric Ponce <ponceeric458@gmail.com>
2023-11-08 17:44:20 -0800

    create dm user list

--- client/src/pages/Home/components/CreateDm.tsx
+++ client/src/pages/Home/components/CreateDm.tsx
@@ -1,5 +1,5 @@
 const CreateDm = () => {
-  const tempArr = new Array(2).fill(0);
+  const tempArr = new Array(10).fill(0);
 
   return (
     <div className="bg-[#313338] h-[400px] w-[440px] absolute left-[210px] shadow-xl border border-[#242628] p-4 z-10">
@@ -14,9 +14,19 @@ const CreateDm = () => {
             placeholder="Type the username"
           />
         </div>
-        <div className="bg-[red] top-[95px]  w-full bottom-[55px] absolute">
-
-        </div>
+        <ul className="top-[90px]  w-full bottom-[55px] absolute overflow-y-auto">
+          {
+            tempArr.map((temp, idx) => {
+              return (
+                <li key={idx} className="h-[42px] w-full flex items-center">
+                  <div className="bg-[red] aspect-square h-[32px] rounded-full mr-3" />
+                  <span className="text-white">Username</span>
+                  <div className="justify-self-end aspect-square bg-[blue] h-4" />
+                </li>
+              )
+            })
+          }
+        </ul>
           <button className="bg-[#5865F2] w-full text-white h-[38px] rounded-sm absolute bottom-0">
             Create DM
           </button>
