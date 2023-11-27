42fecfe9301850f3cc0a9d2aefcc1d9522d4ae34
Eric Ponce <ponceeric458@gmail.com>
2023-11-26 21:05:49 -0800

    user controller routes

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,12 +1,41 @@
 import { Request, Response } from "express";
-
-
+import asyncHandler from "express-async-handler";
+import User from "../models/user.model";
+import DmRequest from "../models/dmRequest.model";
 
 export const testFunc = (req: Request, res: Response) => {
-  return res.json({msg: "Success"})
-}
-
-export const logoutUser = (req: Request, res: Response) => {
-  res.clearCookie("userToken");
-  res.sendStatus(200);
+  return res.json({ msg: "Success" });
 };
+
+export const searchUserByUsername = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const { username, userId } = req.body;
+
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    const request = await DmRequest.findOne({
+      $or: [
+        { sender: user._id, receiver: userId },
+        { sender: userId, receiver: user._id },
+      ],
+    });
+
+    const response = {
+      userId: user._id,
+      username: user.username,
+      status: "NONE",
+    };
+
+    if (!request) {
+      return res.json(response);
+    }
+
+    return res.json({
+      ...response,
+      status: request.sender.toString() === userId ? "PENDING" : "REQUEST",
+    });
+  }
+);
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,10 +1,10 @@
 import { Application } from "express";
-import { logoutUser, testFunc } from "../controllers/user.controller";
+import { testFunc, searchUserByUsername } from "../controllers/user.controller";
 import verifyJWT from "../middleware/verifyJWT.middleware";
 
 const userRoutes = (app: Application) => {
-  app.get("/api/user/logout", logoutUser);
   app.get("/api/user/test", verifyJWT, testFunc);
+  app.post("/api/user/search/username", searchUserByUsername)
 };
 
 export default userRoutes;
