b7ed26da4a5f5a04937e8199adf20216ce89ea60
Eric Ponce <ponceeric458@gmail.com>
2024-02-18 00:12:22 -0800

    server data in side bar

--- client/src/pages/Home/components/ServerChatSidebar.tsx
+++ client/src/pages/Home/components/ServerChatSidebar.tsx
@@ -2,9 +2,8 @@ import { useParams } from "react-router-dom";
 import { useGetAllServerChatsQuery } from "../../../api/serverChatApiSlice";
 
 const ServerChatSidebar = () => {
-  const tempChats = [0, 0, 0, 0];
   const { _serverId } = useParams();
-  const { currentData: serverChats, isLoading } =
+  const { currentData: serverData, isLoading } =
     useGetAllServerChatsQuery(_serverId);
 
   return (
@@ -12,7 +11,7 @@ const ServerChatSidebar = () => {
       {isLoading ? null : (
         <>
           <div className="w-full h-[48px] border-b border-b-[#1F2023] flex items-center justify-center text-left">
-            <span>Server Name</span>
+            <span className="text-white font-medium w-full ml-4">{serverData.title}</span>
           </div>
 
           <ul className="w-full text-[#949BA0] mt-4 px-2">
@@ -23,14 +22,14 @@ const ServerChatSidebar = () => {
               <div className="bg-[blue] h-3 aspect-square" />
             </div>
 
-            {tempChats.map((chat, idx) => {
+            {serverData.textChannels.map((channel: any) => {
               return (
                 <li
                   className="w-full h-[35px] mb-[2px] flex items-center px-2 rounded-sm hover:bg-[#34363b] hover:text-[white] cursor-pointer"
-                  key={idx}
+                  key={channel._id}
                 >
                   <div className="bg-[blue] h-5 aspect-square mr-2 rounded-sm" />
-                  <span>Hello World</span>
+                  <span>{channel.title}</span>
                 </li>
               );
             })}
